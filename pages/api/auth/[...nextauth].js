8bc5fcb3a19b5bc9ebf9491eabfbd3ac8d873f72
andi996 <andi@code.id>
2022-12-27 14:50:28 +0700

    first commit

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,33 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+export default NextAuth({
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      authorizationUrl:
+        "https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code",
+    }),
+  ],
+  jwt: {
+    encryption: true,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt(token, account) {
+      if (account?.accessToken) {
+        token.accessToken = account.accessToken;
+      }
+      console.log(token);
+      console.log(account);
+      return token;
+    },
+    redirect: async (url, _baseUrl) => {
+      if (url === "/profile") {
+        return Promise.resolve("/");
+      }
+      return Promise.resolve("/");
+    },
+  },
+});
