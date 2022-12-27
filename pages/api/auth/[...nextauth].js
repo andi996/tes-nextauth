d457ce5b0ed26573c8016cf9988c90e01ed6be13
andi996 <andi@code.id>
2022-12-27 15:26:06 +0700

    change env

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,16 +4,16 @@ import Providers from "next-auth/providers";
 export default NextAuth({
   providers: [
     Providers.Google({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       authorizationUrl:
         "https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code",
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   jwt: {
     encryption: true,
   },
-  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async jwt(token, account) {
       if (account?.accessToken) {
